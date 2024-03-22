75a32a00eac03be02f9419d4e46640221988174b
Tom Nguyen <tommy.nguyen0721@gmail.com>
2024-03-22 17:21:40 -0400

    WIP

--- listtomap/src/App.js
+++ listtomap/src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Tesseract from 'tesseract.js';
+import { useState } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-function App() {
+const App = () => {
+  const [files, setFiles] = useState([]);
+  const [progress, setProgress] = useState(0);
+  const [result, setResult] = useState([]);
+
+  const filterForAddress = (address) => {
+    // figure out how to manage the list into this array to find addresses
+    const newAddress = address.toLowerCase();
+
+    if (newAddress.indexOf(' st') !== -1 || newAddress.indexOf(' ave') !== -1) {
+      return true;
+    }
+    return false;
+  }
+  // const handleAddMarker = (lon, lat) => {
+  //   addMarker(lon, lat);
+  // }
+  const onFileChange = (e) => {
+    setFiles([...files, e.target.files[0]])
+  }
+
+  const processImage = () => {
+    files.map( file => {
+      Tesseract.recognize(
+        file,
+        "eng",
+        { logger: m => {
+          if(m.status === 'recognizing text') {
+            setProgress(m.progress)
+          }
+        }})
+        .then(({ data: { text } }) => {
+          const newText = text.split('\n')
+          const newList = newText.filter( (address) => {
+            if (address.length > 1 && filterForAddress(address))
+              return address;
+          })
+
+          setResult([...result, ...newList]);
+          return;
+        })
+        return null;
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <input type="file" onChange={onFileChange}/>
+        <div style={{marginTop: 25}}>
+          <input type="button" value="Submit" onClick={processImage} />
+        </div>
+        <div>
+          <progress value={progress} max={1} />
+        </div>
+        <div style={{ marginTop: 20, fontSize: 18 }} >
+          Result:
+        </div>
+        <>
+        { result.length > 0 && result.map( (resultLine, index) => (
+          <>
+            <div>
+              {resultLine}
+            </div>
+            <br />
+          </>
+          ))
+        }
+        </>
+      </div>
+    </>
   );
 }
 
