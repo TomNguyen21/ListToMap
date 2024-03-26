fe5c48e728865124caffd0a1b8d581e9d7404f03
Tommy <tommy.nguyen0721@gmail.com>
2024-03-26 00:57:02 -0400

    Fix CSS for map - WIP

--- listtomap/src/Map.js
+++ listtomap/src/Map.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 
 // Map Embedding and address search stuff
@@ -8,7 +9,7 @@ mapboxgl.accessToken = process.env.REACT_APP_MAPBOX
 const Map = () => {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const addMarker = useRef(null);
+  const addMarkers = useRef(null);
   const [lng, setLng] = useState(-73.9911);
   const [lat, setLat] = useState(40.7359);
   const [zoom, setZoom] = useState(12.5);
@@ -22,14 +23,17 @@ const Map = () => {
       zoom: zoom
     });
   });
+// Do I need to create a redux store to be able to get the results from the image reader? And from there pass that data
+// into this addMarkers function to add markers to the map
+  addMarkers.current = (coordinates) => {
+    coordinates.forEach( (coords) => new mapboxgl.Marker().setLngLat(coords).addTo(map));
 
-  addMarker.current = (coordinates) => {
-    new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+    // new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
   }
 
   return (
     <div>
-        <div ref={mapContainer} className="map-container" />
+        <div ref={mapContainer} className="map-container" addMarkers={addMarkers}/>
     </div>
   )
 };
