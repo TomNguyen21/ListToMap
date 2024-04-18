b471e6ec4bf308b38747fb590910b11abe916125
Tommy <tommy.nguyen0721@gmail.com>
2024-04-18 15:54:36 -0400

    Create FileDrop component

---
+++ listtomap/src/components/fileDrop/FileDrop.js
@@ -0,0 +1,26 @@
+// FileDrop.js
+import React from 'react';
+
+const FileDrop = ({ onFileDrop }) => {
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const files = Array.from(e.dataTransfer.files);
+    onFileDrop(files);
+  };
+
+  return (
+    <div
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      style={{ width: '300px', height: '300px', border: '2px dashed gray', textAlign: 'center' }}
+    >
+      Drop files here
+    </div>
+  );
+};
+
+export default FileDrop;
