2b16f909ae144ce412e38e60d6737ab967882081
Tommy <tommy.nguyen0721@gmail.com>
2024-05-06 21:45:39 -0400

    Import font

--- listtomap/src/App.js
+++ listtomap/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 // eslint-disable-next-line import/no-webpack-loader-syntax
+import './fonts/font.css'
 import Map from './components/map/Map';
 import ImageReader from './features/imageReader/ImageReader';
 
