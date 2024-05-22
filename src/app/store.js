fe28fcfa4000d8fcf432eef813327584aef9a3ee
Tommy <tommy.nguyen0721@gmail.com>
2024-05-22 05:45:28 -0400

    WIP

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import imageReaderReducer from '../features/imageReader/imageReaderSlice';
 
+const defaultMiddlewareConfig = {
+    serializableCheck: false
+  };
+
 const store = configureStore({
     reducer: {
         addresses: imageReaderReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware(defaultMiddlewareConfig),
 })
 
 export default store;
