da28c78a9371322cec5245400edd330f5d4b233f
Tommy <tommy.nguyen0721@gmail.com>
2024-08-01 17:44:15 -0400

    Change CSS to match figma

--- src/features/imageReader/ImageReader.js
+++ src/features/imageReader/ImageReader.js
@@ -18,7 +18,7 @@ const ImageReader = () => {
     const files = useSelector( (state) => state.files.files)
 
     useEffect(() => {
-      console.log({ files });
+      console.log(files);
       files.forEach(previewImg)
     }, [files]);
 
@@ -27,6 +27,7 @@ const ImageReader = () => {
 
     function parseAddresses(text) {
       // Regular expression pattern for matching street addresses
+      //TODO: Still need to refine this so it will get more of the addresses and the names of venues
       const addressPattern = /\b\d{1,5}\s\w+(?:\s\w+)?\s(?:N|S|E|W|NE|NW|SE|SW)?\s?(?:Street|St|Avenue|Ave|Boulevard|Blvd|Road|Rd|Lane|Ln|Drive|Dr|Court|Ct|Circle|Cir|Way|Terrace|Ter|Place|Pl)\b/gi;
 
       // Find all matches in the input text
@@ -78,20 +79,20 @@ const ImageReader = () => {
         img.src = reader.result
         img.className = 'previewImg'
         document.getElementById('gallery').appendChild(img);
-        console.log(files)
       }
     }
 
     return (
       <>
-      { files ? (
+      { files.length ? (
         <div className="App">
-          <div className="Uploader">
+          <FileDrop onFileDrop={handleFileDrop} onFileChange={onFileChange} processImage={processImage} progress={progress}/>
+          {/* <div className="Uploader">
             <FileDrop onFileDrop={handleFileDrop}/>
             <img className='uploadIcon' src={icon} alt='Upload' />
             <span className="uploadMsg">Upload an image and we'll plot the addresses on a map!</span>
             <div className="fileChooser">
-              <input type="file" onChange={onFileChange} multiple/>
+              <input type="file" onChange={onFileChange} multiple hidden/>
             </div>
             <div className="fileSubmit">
               <input className='uploadButton' type="button" value="Upload image" onClick={processImage} />
@@ -100,16 +101,22 @@ const ImageReader = () => {
 
           <div>
             <progress value={progress} max={1} />
-          </div>
-          </div>{/* DEMO IMAGES HERE PROBABLY */}
-          {console.log(files)}
-            {files.length < 1 ? (
+          </div> */}
+          {/* </div>DEMO IMAGES HERE PROBABLY */}
+            {files.length <= 0 ? (
               <div className='demoGallery'>
+                <span className='uploadMsg' >No image? Try one of these:</span>
+                <div style={{display: 'flex', margin: 36}}>
                   <div><img className='demoImg' src={Demo1} alt='Demo' /> </div>
                   <div><img className='demoImg' src={Demo2} alt='Demo' /> </div>
+                </div>
               </div>
             ) :
-            <div id="gallery" /> }
+            <div className="App">
+
+              <div id="gallery" /> 
+            </div>
+            }
         </div>
       ) : (
         //TODO: Create new compnent/feature to render the results and replace with React Component
